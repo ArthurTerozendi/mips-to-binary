774d97df685f8d382358c953b942afffe4c301c1
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-21 16:01:46 -0300

    aplicando as excessões

--- TransformandoMipsEmBinario/src/ERE/Mips/FormatoR.java
+++ TransformandoMipsEmBinario/src/ERE/Mips/FormatoR.java
@@ -7,6 +7,7 @@ public class FormatoR extends Converter {
     private String rd;
     private String rs;
     private String rt;
+    private String shamt;
 
     /**
      * Construtor que irá receber a função e os registradores
@@ -22,19 +23,96 @@ public class FormatoR extends Converter {
      */
     public String separador() {
         String registrador[] = registradores.split(",");
-        rd = registrador[0];
-        rs = registrador[1];
-        rt = registrador[2];
-        System.out.println(rd + " " + rs + " " + rt);
-        return converterParaBinario(rd, rs, rt);
+        if (funcao.equals("mfhi") || funcao.equals("mflo")) {
+            //essas funçôes só tem o rd
+
+            return converterParaBinario(registradores, "00000", "00000", "00000");
+        }
+        else if (funcao.equals("mthi") || funcao.equals("mtlo") || funcao.equals("jr")) {
+            //essas funções só tem o rs
+
+            return converterParaBinario("00000", registradores, "00000", "00000");
+        }
+        else if (funcao.equals("mult") || funcao.equals("multu")
+                || funcao.equals("div") || funcao.equals("divu")) {
+            //essas funções tem só os rs e o rt
+
+            rs = registrador[0];
+            rt = registrador[1];
+
+            return converterParaBinario("00000", rs, rt, "00000");
+        }
+        else if (funcao.equals("jalr")) {
+            //essa função tem só o rs e o rd
+            rd = registrador[0];
+            rs = registrador[1];
+
+            return converterParaBinario(rd, rs, "00000", "00000");
+        }
+        else if (funcao.equals("sra") || funcao.equals("srl") || funcao.equals("sll")) {
+            //essas funções tem o rt, o rd e um numero
+
+            rd = registrador[0];
+            rt = registrador[1];
+            shamt = registrador[2];
+
+            int verificarNegativo = Integer.parseInt(shamt);
+            String binario = Integer.toBinaryString(Integer.parseInt(shamt));
+
+            if (verificarNegativo < 0){
+                binario = preencherBinario(binario, true);
+            } else {
+                binario = preencherBinario(binario, false);
+            }
+
+            shamt = binario;
+
+            return converterParaBinario(rd, "00000", rt, shamt);
+        }
+        else {
+            //essas funções tem rd, rs e o rt
+            rd = registrador[0];
+            rs = registrador[1];
+            rt = registrador[2];
+
+            return converterParaBinario(rd, rs, rt, "00000");
+        }
     }
 
-    private String converterParaBinario(String rd, String rs, String rt) {
+    private String converterParaBinario(String rd, String rs, String rt, String shamt) {
 
         String comandoBinario = "";
 
-        comandoBinario = "000000" + comandos.get(rs) + comandos.get(rt) + comandos.get(rd) + "00000" + comandos.get(funcao);
+        if (shamt.equals("00000")){
+            comandoBinario = "000000"
+                    + comandos.get(rs)
+                    + comandos.get(rt)
+                    + comandos.get(rd)
+                    + comandos.get(shamt)
+                    + comandos.get(funcao);
+        } else {
+            comandoBinario = "000000" + comandos.get(rs) + comandos.get(rt) + comandos.get(rd) + shamt + comandos.get(funcao);
+
+        }
+
 
         return comandoBinario;
+    }
+    private String preencherBinario(String bin, boolean negativo){
+
+        String binSplit[] = bin.split("");
+
+        if (negativo) {
+            bin = "";
+            for (int i = 27; i < binSplit.length; i++) {
+                bin += binSplit[i];
+            }
+        } else {
+            for (int i = 0; i < 5 - binSplit.length ; i++) {
+                bin = "0" + bin;
+            }
+        }
+
+        return bin;
     }
 }
